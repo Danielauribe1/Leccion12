2cf8b12428ca93a2591a2e5cb56ea291ad6fdf8c
DUM <114031187+Danielauribe1@users.noreply.github.com>
2023-01-17 15:42:20 -0500

    Quinto avance enero JS

---
+++ 09-05Continue.js
@@ -0,0 +1,8 @@
+// Sentencia continue
+
+for(let contador = 0; contador <= 10; contador++){
+    if( contador % 2 !== 0){
+        continue;//ir a la siguiente iteracion
+    }
+    console.log(contador);
+}
